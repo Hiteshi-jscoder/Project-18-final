3c03756c9da85ca2802c848e9cf85ff0e7699b5d
Hiteshi Singla <72081322+Hiteshi-jscoder@users.noreply.github.com>
2020-11-07 16:31:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var banaImage,obstacleImg,backImg,score,back;
+var obstaclegrp;
+
+function preload(){
+  obstacleImg=loadImage("stone.png");
+  banaImg=loadImage("banana.png");
+  backImg=loadImage("jungle.jpg");
+}
+
+function setup() {
+  createCanvas(600,300);
+  
+  back=createSprite(300,150,600,300);
+  obstaclegrp=new Group();
+}
+
+function draw() {
+  background("blue");
+  
+  drawSprites();
+}
